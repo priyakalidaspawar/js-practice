e54a822cd4472e20867f52659daed6314ac4dfc5
priyakalidaspawar <92677580+priyakalidaspawar@users.noreply.github.com>
2024-02-20 09:18:02 +0000

    String to number conversion

--- js_basics/conversionOperation.js
+++ js_basics/conversionOperation.js
@@ -1,4 +1,8 @@
+// *****************CONVERSION*************************
+
 let age = "false"
+
+
 
 // console.log(typeof age);
 
@@ -21,5 +25,43 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let number = 10
 let stringNo = String(number)
-console.log(stringNo);
-console.log(typeof stringNo);
+// console.log(stringNo);
+// console.log(typeof stringNo);
+
+//************* OPERATIONS ******************
+
+let value = 5
+let negValue = -value
+// console.log(value);
+// console.log(negValue);
+
+// console.log(2+2); // 4
+// console.log(2-2); // 0
+// console.log(2*2); // 4
+// console.log(2**3); // 8
+// console.log(2/3); // 0.6666666
+// console.log(2%3); // 2
+
+let str1 = "Hello"
+let str2 = " World"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2); //12
+// console.log(1 + "2"); //12
+// console.log("1" + 2 + 2); // 122
+// console.log(1 + 2 + "2"); // 32
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(true); // true
+// console.log(+true); //1
+// // console.log(true+); //error
+// console.log(+""); // 0
+
+let n1 , n2,  n3
+n1 = n2 = n3 = 2 + 2
+
+let Counter = 50
+Counter++;
+console.log(Counter); // 51
