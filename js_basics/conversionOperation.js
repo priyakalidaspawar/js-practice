3ab7eb4c98ca399d5f70a69f817a1f3b861fbb08
priyakalidaspawar <92677580+priyakalidaspawar@users.noreply.github.com>
2024-02-20 08:58:51 +0000

    Conversion of Data Types

---
+++ js_basics/conversionOperation.js
@@ -0,0 +1,25 @@
+let age = "false"
+
+// console.log(typeof age);
+
+let valueInNumber = Number(age)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN(Not a Number)
+// true => 1; false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true ; 0 => false
+// " " => false
+// "Hi"=> true
+
+let number = 10
+let stringNo = String(number)
+console.log(stringNo);
+console.log(typeof stringNo);
