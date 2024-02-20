cef2055aa71accac11de4e79a8e6e5e14f7e5d59
priyakalidaspawar <92677580+priyakalidaspawar@users.noreply.github.com>
2024-02-20 07:07:18 +0000

    datatypes

---
+++ js_basics/dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+//console.log(3+3); console.log("Hii") //Bad code practice
+
+// console.log(3+3)
+// console.log("Hii") //readable code
+
+let name = "Priya"
+let age = 22
+let isLoggedIn = false
+
+//Data Types-->
+
+// numer => 2 to power of 53
+// bigint
+// string => ' ' or " "
+// boolean => true/false
+// null =>standalone value
+// undefined
+// symbol => unique
+
+//object
+
+console.log(typeof "Hi"); //string 
+console.log(typeof age); // number
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
