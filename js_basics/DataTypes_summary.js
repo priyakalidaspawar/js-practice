bbc229d004030ce235dad831a583ece29bbe98e5
priyakalidaspawar <92677580+priyakalidaspawar@users.noreply.github.com>
2024-02-21 10:06:45 +0000

    datatype summary

---
+++ js_basics/DataTypes_summary.js
@@ -0,0 +1,43 @@
+//  Primitive Data Types
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId); //false
+
+console.log(id); // Symbol(123)
+console.log(anotherId); // Symbol(123)
+
+
+const bigNumber = 3456543576654356754n
+
+// Reference (Non primitive) datatype
+
+// Array, Objects, Functions
+
+const fruits = ["apple", "orange", "banana"]; //array
+let myObj = {
+    name: "hitesh",
+    age: 22,
+} //object
+
+const myFunction = function(){
+    console.log("Hello world"); // function 
+}
+
+console.log(typeof bigNumber); //bigint
+console.log(typeof outsideTemp); //object
+console.log(typeof myFunction); //function
+console.log(typeof fruits); //object
+console.log(typeof myObj); //object
+console.log(typeof anotherId); //symbol
+ 
