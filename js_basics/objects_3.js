d9eac09aa8488d19a90ae960af0730df137be933
priyakalidaspawar <92677580+priyakalidaspawar@users.noreply.github.com>
2024-02-29 09:02:38 +0000

    objects

---
+++ js_basics/objects_3.js
@@ -0,0 +1,13 @@
+// De-structure
+
+const college = {
+    name: "COEP",
+    fee: "9999",
+    course: "CS"
+}
+
+// const {name} = college
+// console.log(name); // COEP
+
+// const {name: n} = college
+// console.log(n); // COEP
